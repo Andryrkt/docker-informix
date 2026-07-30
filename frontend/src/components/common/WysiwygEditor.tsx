6278c6cb4d7dc1cd4babe245c6fe249a2337d1af
Jaina20 <87335072+Jaina20@users.noreply.github.com>
2026-07-30 11:55:47 +0300

    rectif UI: rectification focus details demande

--- frontend/src/components/common/WysiwygEditor.tsx
+++ frontend/src/components/common/WysiwygEditor.tsx
@@ -69,7 +69,11 @@ export default function WysiwygEditor({
   return (
     <div
       className={cn(
-        " field-sizing-content min-h-16 w-full rounded-sm border border-input bg-transparent  text-xs transition-colors outline-none placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-1 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-1 aria-invalid:ring-destructive/20 md:text-xs dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40",
+        "field-sizing-content min-h-16 w-full rounded-sm border border-input bg-transparent text-xs transition-colors outline-none placeholder:text-muted-foreground",
+        "focus-within:border-ring focus-within:ring-1 focus-within:ring-ring/50", // 👈
+        "disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50",
+        "aria-invalid:border-destructive aria-invalid:ring-1 aria-invalid:ring-destructive/20",
+        "dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40",
         className,
       )}
     >
